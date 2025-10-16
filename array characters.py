09c4eeba8f3deb3dab41f6145b8bb2445be2ed39
Deepthi-varsha <bdeepthivarsha@gmail.com>
2025-10-16 15:45:15 +0530

    Add files via upload

---
+++ array characters.py
@@ -0,0 +1,6 @@
+input_string = "Learning Python is fun!"
+char_array = []
+# Loop through each character in the string
+for char in input_string:
+ char_array.append(char)
+print("Array of characters:", char_array) 
